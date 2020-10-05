70f74ce28c82d7f045addbadd6b2fa0dbbad766e
steppam <steppam@mail.ru>
2020-10-05 13:49:11 +0300

    empty solution

---
+++ matyshchak/VMindbooke.Bot/VMindbooke.Bot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VMindbooke.Bot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
