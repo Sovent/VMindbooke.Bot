d504613990c160558b4868a8e321a3f37b51f774
Ilya <ilyaaxenov1999@gmail.com>
2020-10-09 02:47:37 +0300

    application configured

--- aksenov/Application/Program.cs
+++ aksenov/Application/Program.cs
@@ -1,5 +1,10 @@
 ﻿using System;
+using Hangfire;
+using Hangfire.MemoryStorage;
+using Serilog;
 using VMindbooke.Bot;
+using VMindbooke.Bot.Application;
+using VMindbooke.Bot.Infrastructure;
 
 namespace Application
 {
@@ -7,11 +12,36 @@ namespace Application
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-
             var settings = BotSettings.FromJsonFile("appsettings.json");
+            
+            var logger = new LoggerConfiguration()
+                .WriteTo.File("regular.log")
+                .WriteTo.Console()
+                .CreateLogger();
 
-            Console.WriteLine();
+            var apiRequestsService = new APIRequestsService(settings, logger);
+            
+            var service = new LikeBoostingService(settings, 
+                apiRequestsService, 
+                DateTime.Now,
+                new SpamRepository(), 
+                new HashesRepository(), 
+                new ProcessedObjectsRepository(), 
+                logger);
+
+            GlobalConfiguration.Configuration.UseMemoryStorage();
+            RecurringJob.AddOrUpdate(() => service.BoostFinishScenario(), "*/5 * * * *");
+            RecurringJob.AddOrUpdate(() => service.CommentsWritingScenario(), "*/10 * * * *");
+            RecurringJob.AddOrUpdate(() => service.RepliesWritingScenario(), "*/10 * * * *");
+            RecurringJob.AddOrUpdate(() => service.PostsCopyingByLikesScenario(), "*/10 * * * *");
+            RecurringJob.AddOrUpdate(() => service.PostsCopyingByUsersScenario(), "*/10 * * * *");
+
+            using (var backgroundServer = new BackgroundJobServer())
+            {
+                logger.Information("Like boosting started.");
+                Console.WriteLine("Press any key to finish boosting.");
+                Console.ReadKey();
+            }
         }
     }
 }
